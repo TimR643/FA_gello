2b6c166b0a7a52f0c4632d0e3539c38398ba7a5d
TimR643 <tim@ingruk.de>
2026-06-29 17:41:44 +0200

    new task prepared

--- check_gello_start_position.sh
+++ check_gello_start_position.sh
@@ -10,7 +10,7 @@ conda activate "${LEROBOT_ENV:-$HOME/miniconda3/envs/lerobot}"
 REPO_DIR="${GELLO_REPO_DIR:-$HOME/gello_software}"
 cd "$REPO_DIR"
 
-TARGET_ARGS=(--target-rad "${START_JOINTS_RAD:-0.0905,-0.1567,-0.1225,-2.3384,-0.0876,2.1658,-0.7721}")
+TARGET_ARGS=(--target-rad "${START_JOINTS_RAD:-0.0419,-0.1546,-0.0701,-2.2596,-0.0772,2.1024,-0.7830}")
 
 python scripts/check_gello_start_position.py \
   --robot-host "${ROBOT_HOST:-127.0.0.1}" \
